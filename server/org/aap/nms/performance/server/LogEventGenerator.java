1941478bb0144c9ebb5c0a98256383b1ce2d49ae
Andrew Porokhin <andrew.porokhin@gmail.com>
2009-01-18 04:42:32 +0300

    Update performance test (sleep, formating);

--- server/org/aap/nms/performance/server/LogEventGenerator.java
+++ server/org/aap/nms/performance/server/LogEventGenerator.java
@@ -27,10 +27,10 @@ public class LogEventGenerator extends StandartODObject implements Runnable {
     private static final String FULLNAME = "Log Event Generator";
     private static final String VERSION = "0.0";
     private static final String COPYRIGHT = "(C) 2008 Andrew Porokhin";
-    private static final String DEVICE_NAME = null;
+    private static final String DEVICE_NAME = "one";
     
     /** Report about progress every 10 second. */
-    private static final long REPORT_TIME = 10000;
+    private static final long REPORT_TIME = 5000;
     /** Worker thread. */
     private Thread worker;
 
@@ -56,7 +56,7 @@ public class LogEventGenerator extends StandartODObject implements Runnable {
     public String[] getDepends() {
         String[] result = { "dispatcher",
                 com.novel.nms.server.storage.Storage.class.getName(),
-                com.novel.nms.server.devices.common.DeviceList.class.getName(),
+                // com.novel.nms.server.devices.common.DeviceList.class.getName(),
         };
         return result;
     }
@@ -70,28 +70,33 @@ public class LogEventGenerator extends StandartODObject implements Runnable {
     }
 
     public void run() {
-        Calendar c = Calendar.getInstance(TimeZone.getDefault());
         long logEventSent = 0;
         long logEventSentReport = 0;
         
+        Calendar c = Calendar.getInstance(TimeZone.getDefault());
         long initialTime = c.getTimeInMillis();
         long currentTime = initialTime;
         long lastReportTime = currentTime;
         logger.info("Event generator thread started.");
         while (!Thread.interrupted()) {
+            c = Calendar.getInstance(TimeZone.getDefault());
             currentTime = c.getTimeInMillis();
             Long date = new Long(currentTime / 1000);
             Message m = dispatcher.getNewMessage();
             LogEventMessage.setup(m, "nmslog", getObjectName(), UUID.getNullUUID());
+            
             LogEventMessage.setName(m, DEVICE_NAME);
             LogEventMessage.setSource(m, DEVICE_NAME);
             LogEventMessage.setDate(m, date);
+            
             LogEventMessage.setLDate(m, date);
             LogEventMessage.setIndex(m, Device.POLL_TIMEOUT_EVENT_IDX);
             LogEventMessage.setEvent(m, LogEventMessage.ALARM_POLL_TIMEOUT);
+            
             LogEventMessage.setObject(m, "");
             LogEventMessage.setPC(m, "Poll timeout");
             LogEventMessage.setAI(m, "");
+            
             LogEventMessage.setEquipStatus(m, LogEventMessage.ALARM_POLL_TIMEOUT);
             m.setRoutable(false);
             dispatcher.send(m);
@@ -100,14 +105,24 @@ public class LogEventGenerator extends StandartODObject implements Runnable {
             
             if (logger.isLoggable(Level.INFO)) {
                 if (currentTime - lastReportTime > REPORT_TIME) {
-                    logger.info("Messages: " + logEventSentReport + " in " + (currentTime - lastReportTime) + " ms");
-                    logger.info("Rate aprox.: " + (logEventSentReport/(currentTime - lastReportTime)/1000) + " log per second");
-                    
-                    logger.info("Total messages: " + logEventSent + " in " + (currentTime - initialTime) + " ms");
-                    logger.info("Global rate aprox.: " + (logEventSent/(currentTime - initialTime)/1000) + " log per second");
+                    logger.info("LOG GENERATOR REPORT...\n"
+                     + "Elapsed: " + (currentTime - lastReportTime) + " msec\n"
+                     + "Messages: " + logEventSentReport + " in " + (currentTime - lastReportTime) + " ms\n"
+                     + "Rate aprox.: " + (logEventSentReport/((currentTime - lastReportTime)/1000)) + " log per second\n"
+                     + "Total messages: " + logEventSent + " in " + (currentTime - initialTime) + " ms\n"
+                     + "Global rate aprox.: " + (logEventSent/((currentTime - initialTime)/1000)) + " log per second");
                     
                     logEventSentReport = 0;
                     lastReportTime = currentTime;
+                }
+            }
+            
+            c = Calendar.getInstance(TimeZone.getDefault());
+            if (c.getTimeInMillis() - currentTime < 1) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
